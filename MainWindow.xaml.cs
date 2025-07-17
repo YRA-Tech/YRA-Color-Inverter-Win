5b444aec78040724e4c8aaaf434db051b4e42fb2
Jeff Witt <witt.jeff@gmail.com>
2025-07-17 13:02:55 -0400

    fixed build error

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
